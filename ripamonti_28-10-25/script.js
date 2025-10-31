941dd9356f06b3dd61a0dae1874ac031cf0f4c51
DANIELE RIPAMONTI <ripamonti.daniele.08@itisdalmine.edu.it>
2025-10-31 11:58:22 +0100

    finito esercizio 1

--- ripamonti_28-10-25/script.js
+++ ripamonti_28-10-25/script.js
@@ -1,10 +1,7 @@
-//NON FUNZIONA
-
 const nomeInput = document.getElementById("nome");
 const emailInput = document.getElementById("email");
-
-nomeInput.preventDefault();
-emailInput.preventDefault();
+const form = document.getElementById("form");
+esito = document.getElementById("esito");
 
 function addFocusStyle(input) {
     input.addEventListener("focus", () => {
@@ -13,8 +10,38 @@ function addFocusStyle(input) {
 
     input.addEventListener("blur", () => {
         input.classList.remove("is-focused");
+        if (!input.value.trim()) {
+          input.classList.add('err');
+        };
+    });
+
+    input.addEventListener('input', () => {
+        input.classList.remove('err');
+        input.classList.remove('accepted');
     });
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (!emailInput.checkValidity()) {
+        esito.textContent = 'Email non valida.';
+        emailInput.classList.add('err');
+    }
+    else if (nome.value.trim() && email.value.trim()) {
+        esito.textContent = 'Registrazione completata con successo!';
+        nomeInput.classList.add('accepted');
+        emailInput.classList.add('accepted');
+    }
+    else {
+        esito.textContent = 'Compila tutti i campi.';
+        if (!nome.value.trim()) {
+            nomeInput.classList.add('err');
+        }
+        if (!email.value.trim()) {
+            emailInput.classList.add('err');
+        }
+    }
+});
 
 addFocusStyle(nomeInput);
 addFocusStyle(emailInput);
